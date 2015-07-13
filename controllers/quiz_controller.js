0da29bd1db092e49d26d5f04472d71fb587a4283
Rafa Garrido <rgarom@gmail.com>
2015-07-13 23:45:35 +0200

    Autores

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -8,5 +8,8 @@ exports.answer = function(req, res) {
   } else {
     res.render('quizes/answer', {respuesta: 'Incorrecto'});
   }
+};
 
+exports.author = function(req, res) {
+  res.render('author', {autor: 'Rafael Garrido Romero'});
 };
