1275ebe990c12b56d3c474150390e6d21cc871d0
Rafa Garrido <rgarom@gmail.com>
2015-08-02 12:33:12 +0200

    modulo 8

---
+++ model/quiz.js
@@ -0,0 +1,14 @@
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    'Quiz',
+    { pregunta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Pregunta"} }
+    },
+      respuesta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+      }
+    }
+  );
+}
